c198c20b519708138b27c5f37e4d8bf511439b5e
julyecho0701@gmail.com <hshao@meteorcomm.com>
2021-05-23 00:33:51 -0700

    blogging/admin.py

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import Http404
 from blogging.models import Post
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 def list_view(request):
-    context = {'post': Post.objects.all()}
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type="text/html")
     return render(request, 'blogging/list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
