bc2c96c7b45473135dc7cb45a7666bd95327e16c
Hua Shao <hshao@meteorcomm.com>
2021-05-30 00:50:02 -0700

    add change for post

--- blogging/views.py
+++ blogging/views.py
@@ -2,22 +2,15 @@ from django.shortcuts import render
 from blogging.models import Post
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
 
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+    queryset = Post.objects.filter(published_date__exact=None)
+    queryset = Post.objects.order_by('-published_date')
