f1445b4fea5ac83bb5e1f065247e1e937241abf9
Hua Shao <hshao@meteorcomm.com>
2021-06-07 00:27:19 -0700

    apply change

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from rest_framework import routers
+from blogging import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'posts', views.PostViewSet)
 
 urlpatterns = [
     path("polling/", include("polling.urls")),
@@ -25,6 +30,6 @@ urlpatterns = [
     path("", include("blogging.urls")),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
-    path("api/", include(routers.urls)),
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
