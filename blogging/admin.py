76881a4eb16a60c642efc37ac066e6eb395a5c72
Hua Shao <hshao@meteorcomm.com>
2021-06-06 18:51:28 -0700

    Applied black.

--- blogging/admin.py
+++ blogging/admin.py
@@ -5,17 +5,18 @@ from blogging.models import Post, Category
 class PostsInline(admin.TabularInline):
     model = Category.posts.through
 
+
 class PostAdmin(admin.ModelAdmin):
-    fields = ('title', 'text', 'author')
-    exclude = ('created_date', 'modified_date', 'published_date')
+    fields = ("title", "text", "author")
+    exclude = ("created_date", "modified_date", "published_date")
     inlines = [
         PostsInline,
     ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    fields = ('name', 'description')
-    exclude = ('posts',)
+    fields = ("name", "description")
+    exclude = ("posts",)
 
 
 admin.site.register(Post, PostAdmin)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),
+    path("polling/", include("polling.urls")),
     # path('blogging/', include('blogging.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('blogging.urls')),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path("admin/", admin.site.urls),
+    path("", include("blogging.urls")),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
